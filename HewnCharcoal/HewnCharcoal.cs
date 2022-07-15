d430dc3687850748ac4cfdab202202cb627b200e
Lynn <firemagelynn@gmail.com>
2022-07-14 22:51:57 -0400

    lint

--- HewnCharcoal/HewnCharcoal.cs
+++ HewnCharcoal/HewnCharcoal.cs
@@ -20,33 +20,33 @@ using Eco.Gameplay.Pipes;
 
 namespace Eco.Mods.TechTree
 {
-  [RequiresSkill(typeof(LoggingSkill), 4)]
-  public partial class HewnCharcoalRecipe : RecipeFamily
-  {
-    public HewnCharcoalRecipe()
+    [RequiresSkill(typeof(LoggingSkill), 4)]
+    public partial class HewnCharcoalRecipe : RecipeFamily
     {
-      this.Recipes = new List<Recipe> {
-				new Recipe(
-					"HewnCharcoal",
-					Localizer.DoStr("Hewn Charcoal"),
-					new IngredientElement[] {
-						new IngredientElement("HewnLog", 7, typeof(LoggingSkill)),
-					},
-					new CraftingElement[] {
-						new CraftingElement<CharcoalItem>()
-					}
-				)
-			};
+        public HewnCharcoalRecipe()
+        {
+            this.Recipes = new List<Recipe>
+            {
+                new Recipe(
+                    "HewnCharcoal",
+                    Localizer.DoStr("Hewn Charcoal"),
+                    new IngredientElement[]
+                    {
+                        new IngredientElement("HewnLog", 7, typeof(LoggingSkill)),
+                    },
+                    new CraftingElement[] { new CraftingElement<CharcoalItem>() }
+                )
+            };
 
-      this.ExperienceOnCraft = 1;
-      this.LaborInCalories = CreateLaborInCaloriesValue(25, typeof(LoggingSkill));
-      this.CraftMinutes = CreateCraftTimeValue(
-          typeof(HewnCharcoalRecipe),
-          1,
-          typeof(LoggingSkill)
-      );
-      this.Initialize(Localizer.DoStr("Hewn Charcoal"), typeof(HewnCharcoalRecipe));
-      CraftingComponent.AddRecipe(typeof(KilnObject), this);
+            this.ExperienceOnCraft = 1;
+            this.LaborInCalories = CreateLaborInCaloriesValue(25, typeof(LoggingSkill));
+            this.CraftMinutes = CreateCraftTimeValue(
+                typeof(HewnCharcoalRecipe),
+                1,
+                typeof(LoggingSkill)
+            );
+            this.Initialize(Localizer.DoStr("Hewn Charcoal"), typeof(HewnCharcoalRecipe));
+            CraftingComponent.AddRecipe(typeof(KilnObject), this);
+        }
     }
-  }
 }
